46f0452413b694e0b6d85d8869d41f44f03565a7
Nils Weigel <n.weigel@neusta.de>
2022-07-14 11:05:56 +0200

    updated root config name

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -9,7 +9,7 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('presentation');
+        $treeBuilder = new TreeBuilder('neusta_pimcore_presentation');
         return $treeBuilder;
     }
 }
