2c658f01dc54c7843bff8d5b5054cf27974e8c27
Luka Dschaak <l.dschaak@neusta.de>
2022-11-11 08:51:29 +0100

    add test for checking the symfony service definitions

---
+++ tests/Integration/ServiceCompilerTest.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+namespace Neusta\Pimcore\PresentationBundle\Tests\Integration;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class ServiceCompilerTest extends KernelTestCase
+{
+    protected function setUp(): void
+    {
+        self::bootKernel(['environment' => 'test']);
+    }
+
+    /** @test */
+    public function symfonyServiceDefinitionsMustCompile(): void
+    {
+        // when this test passed, it means that the kernel could be loaded and there are no compiling errors in the
+        // symfony service definitions.
+        $this->expectNotToPerformAssertions();
+    }
+}
