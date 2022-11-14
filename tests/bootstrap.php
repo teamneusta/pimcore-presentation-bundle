7108021fc6713a17d3a9b6a5d24a3870b707efad
Luka Dschaak <l.dschaak@neusta.de>
2022-11-14 12:11:13 +0100

    make php-cs-fixer happy

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,17 +5,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $pimcoreProjectRootDir = __DIR__ . '/app';
 $_SERVER['PIMCORE_PROJECT_ROOT'] = $pimcoreProjectRootDir;
 $_ENV['PIMCORE_PROJECT_ROOT'] = $pimcoreProjectRootDir;
-putenv("PIMCORE_PROJECT_ROOT=" . $pimcoreProjectRootDir);
+putenv('PIMCORE_PROJECT_ROOT=' . $pimcoreProjectRootDir);
 
 $kernelClass = 'TestKernel';
 $_SERVER['KERNEL_CLASS'] = $kernelClass;
 $_ENV['KERNEL_CLASS'] = $kernelClass;
-putenv("KERNEL_CLASS=" . $kernelClass);
+putenv('KERNEL_CLASS=' . $kernelClass);
 
 $appEnv = 'test';
 $_SERVER['APP_ENV'] = $appEnv;
 $_ENV['APP_ENV'] = $appEnv;
-putenv("APP_ENV=" . $appEnv);
+putenv('APP_ENV=' . $appEnv);
 
 \Pimcore\Bootstrap::setProjectRoot();
 \Pimcore\Bootstrap::bootstrap();
