8b06acde27e93daaf4dfaffda53e97c7aa7dd8aa
Luka Dschaak <l.dschaak@neusta.de>
2022-11-15 09:59:55 +0100

    review changes

--- tests/Integration/ServiceCompilerTest.php
+++ tests/Integration/ServiceCompilerTest.php
@@ -6,16 +6,11 @@ use Pimcore\Test\KernelTestCase;
 
 class ServiceCompilerTest extends KernelTestCase
 {
-//    protected function setUp(): void
-//    {
-//        self::bootKernel(['environment' => 'test']);
-//    }
-
     /** @test */
     public function symfonyServiceDefinitionsMustCompile(): void
     {
-        // when this test passed, it means that the kernel could be loaded and there are no compiling errors in the
-        // symfony service definitions.
+        // when this test passed, it means that the kernel could be loaded (boostrap.php) and there are no
+        // compiling errors in the symfony service definitions.
         $this->expectNotToPerformAssertions();
     }
 }
--- tests/app/TestKernel.php
+++ tests/app/TestKernel.php
@@ -4,10 +4,6 @@ use Neusta\Pimcore\PresentationBundle\NeustaPimcorePresentationBundle;
 use Pimcore\HttpKernel\BundleCollection\BundleCollection;
 use Pimcore\Kernel;
 
-/**
- * Adapted by https://github.com/bernardosecades/tips-and-tricks/tree/master/functional-test-standalone-bundle
- * in the path Tests/AppKernel.php.
- */
 class TestKernel extends Kernel
 {
     public function registerBundlesToCollection(BundleCollection $collection): void
