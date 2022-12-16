2057bc347c0cf8328e51e4682115cd8f4143ff4a
storbahn <s.torbahn@neusta.de>
2022-12-16 13:35:21 +0100

    [#6] Installer: Add check if bundle is installed
    Closes #6

--- src/Installer.php
+++ src/Installer.php
@@ -3,25 +3,45 @@
 namespace Neusta\Pimcore\PresentationBundle;
 
 use Pimcore\Extension\Bundle\Installer\AbstractInstaller;
-use Pimcore\Model\Asset\Image\Thumbnail;
+use Pimcore\Model\Asset\Image\Thumbnail\Config as ThumbnailConfig;
 use Pimcore\Model\Document\DocType;
 
 class Installer extends AbstractInstaller
 {
+    private const THUMBNAIL_CONFIG_BACKGROUND_IMAGE = 'pimcore-presentation-bundle-background-image';
+
     public function install(): void
     {
         $this->installDocumentTypes();
         $this->installThumbnailConfiguration();
     }
 
+    public function uninstall(): void
+    {
+        $this->uninstallDocumentTypes();
+        $this->uninstallThumbnailConfiguration();
+    }
+
     public function isInstalled(): bool
     {
-        return false;
+        $documentTypeDefinitions = $this->getDocumentTypeDefinitions();
+        foreach ($documentTypeDefinitions as $documentTypeDefinition) {
+            if (!DocType::getById($documentTypeDefinition['id'])) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public function canBeInstalled(): bool
     {
         return !$this->isInstalled();
+    }
+
+    public function canBeUninstalled(): bool
+    {
+        return $this->isInstalled();
     }
 
     public function needsReloadAfterInstall(): bool
@@ -31,14 +51,19 @@ class Installer extends AbstractInstaller
 
     private function installDocumentTypes(): void
     {
-        $typeDefinitionsFile =
-            __DIR__ . \DIRECTORY_SEPARATOR .
-            'Resources' . \DIRECTORY_SEPARATOR .
-            'config' . \DIRECTORY_SEPARATOR .
-            'document-types.php';
-        $typeDefinitions = include $typeDefinitionsFile;
-        foreach ($typeDefinitions as $typeDefinition) {
-            $this->installDocumentType($typeDefinition);
+        $documentTypeDefinitions = $this->getDocumentTypeDefinitions();
+        foreach ($documentTypeDefinitions as $documentTypeDefinition) {
+            $this->installDocumentType($documentTypeDefinition);
+        }
+    }
+
+    private function uninstallDocumentTypes(): void
+    {
+        $documentTypeDefinitions = $this->getDocumentTypeDefinitions();
+        foreach ($documentTypeDefinitions as $documentTypeDefinition) {
+            if ($docType = DocType::getById($documentTypeDefinition['id'])) {
+                $docType->delete();
+            }
         }
     }
 
@@ -53,31 +78,58 @@ class Installer extends AbstractInstaller
      *     'priority':int,
      *     'creationDate':int,
      *     'modificationDate':int
-     * } $typeDefinition
+     * } $documentTypeDefinition
      */
-    private function installDocumentType(array $typeDefinition): void
+    private function installDocumentType(array $documentTypeDefinition): void
     {
         $model = new DocType();
-        $model->setId($typeDefinition['id']);
-        $model->setName($typeDefinition['name']);
-        $model->setGroup($typeDefinition['group']);
-        $model->setController($typeDefinition['controller']);
-        $model->setTemplate($typeDefinition['template']);
-        $model->setType($typeDefinition['type']);
-        $model->setPriority($typeDefinition['priority']);
-        $model->setCreationDate($typeDefinition['creationDate']);
-        $model->setModificationDate($typeDefinition['modificationDate']);
+        $model->setId($documentTypeDefinition['id']);
+        $model->setName($documentTypeDefinition['name']);
+        $model->setGroup($documentTypeDefinition['group']);
+        $model->setController($documentTypeDefinition['controller']);
+        $model->setTemplate($documentTypeDefinition['template']);
+        $model->setType($documentTypeDefinition['type']);
+        $model->setPriority($documentTypeDefinition['priority']);
+        $model->setCreationDate($documentTypeDefinition['creationDate']);
+        $model->setModificationDate($documentTypeDefinition['modificationDate']);
         $model->save();
     }
 
     private function installThumbnailConfiguration(): void
     {
-        if (Thumbnail\Config::exists('pimcore-presentation-bundle-background-image')) {
+        if (ThumbnailConfig::exists(self::THUMBNAIL_CONFIG_BACKGROUND_IMAGE)) {
             return;
         }
 
-        $thumbnailConfig = new Thumbnail\Config();
-        $thumbnailConfig->setName('pimcore-presentation-bundle-background-image');
+        $thumbnailConfig = new ThumbnailConfig();
+        $thumbnailConfig->setName(self::THUMBNAIL_CONFIG_BACKGROUND_IMAGE);
         $thumbnailConfig->save();
+    }
+
+    private function uninstallThumbnailConfiguration(): void
+    {
+        if ($thumbnailConfig = ThumbnailConfig::getByName(self::THUMBNAIL_CONFIG_BACKGROUND_IMAGE)) {
+            $thumbnailConfig->delete();
+        }
+    }
+
+    /**
+     * @return array<int, array{
+     *     'id':string,
+     *     'name':string,
+     *     'group':string,
+     *     'controller':string,
+     *     'template':string,
+     *     'type':string,
+     *     'priority':int,
+     *     'creationDate':int,
+     *     'modificationDate':int
+     * }>
+     */
+    private function getDocumentTypeDefinitions(): array
+    {
+        return require dirname(__DIR__) . \DIRECTORY_SEPARATOR .
+            'config' . \DIRECTORY_SEPARATOR .
+            'document-types.php';
     }
 }
