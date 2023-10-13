eead0a3cca6f1f527e27166707331e9ac2361417
Luka Dschaak <l.dschaak@neusta.de>
2023-10-13 18:13:18 +0200

    update: add pimcore/admin-ui-classic-bundle to pipeline

--- src/NeustaPimcorePresentationBundle.php
+++ src/NeustaPimcorePresentationBundle.php
@@ -4,22 +4,16 @@ namespace Neusta\Pimcore\PresentationBundle;
 
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 use Pimcore\Extension\Bundle\Installer\InstallerInterface;
+use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
 use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 
-class NeustaPimcorePresentationBundle extends AbstractPimcoreBundle
+class NeustaPimcorePresentationBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
 {
     use PackageVersionTrait;
 
     public function getPath(): string
     {
         return \dirname(__DIR__);
-    }
-
-    public function getJsPaths(): array
-    {
-        return [
-            '/bundles/presentation/js/pimcore/startup.js',
-        ];
     }
 
     public function getInstaller(): ?InstallerInterface
@@ -30,5 +24,27 @@ class NeustaPimcorePresentationBundle extends AbstractPimcoreBundle
     protected function getComposerPackageName(): string
     {
         return 'teamneusta/pimcore-presentation-bundle';
+    }
+
+    public function getJsPaths(): array
+    {
+        return [];
+    }
+
+    public function getCssPaths(): array
+    {
+        return [
+            '/bundles/presentation/js/pimcore/startup.js',
+        ];
+    }
+
+    public function getEditmodeJsPaths(): array
+    {
+        return [];
+    }
+
+    public function getEditmodeCssPaths(): array
+    {
+        return [];
     }
 }
