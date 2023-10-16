fe620be2c962e3b65d8f0cd78955504844e62624
Luka Dschaak <l.dschaak@neusta.de>
2023-10-16 10:03:45 +0200

    refactor: move asset loading to listener

---
+++ src/EventListener/AdminAssetsListener.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+namespace Neusta\Pimcore\PresentationBundle\EventListener;
+
+use Pimcore\Event\BundleManager\PathsEvent;
+use Pimcore\Event\BundleManagerEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class AdminAssetsListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            BundleManagerEvents::JS_PATHS => 'onJsPaths'
+        ];
+    }
+
+    public function onJsPaths(PathsEvent $event): void
+    {
+        $event->setPaths(array_merge($event->getPaths(), [
+            '/bundles/presentation/js/pimcore/startup.js'
+        ]));
+    }
+}
--- src/NeustaPimcorePresentationBundle.php
+++ src/NeustaPimcorePresentationBundle.php
@@ -5,90 +5,23 @@ namespace Neusta\Pimcore\PresentationBundle;
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 use Pimcore\Extension\Bundle\Installer\InstallerInterface;
 use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
-use Pimcore\Version;
 
-if (!method_exists(Version::class, 'getMajorVersion') || 10 === Version::getMajorVersion()) {
-    class NeustaPimcorePresentationBundle extends AbstractPimcoreBundle
+class NeustaPimcorePresentationBundle extends AbstractPimcoreBundle
+{
+    use PackageVersionTrait;
+
+    public function getPath(): string
     {
-        use PackageVersionTrait;
-
-        public function getPath(): string
-        {
-            return \dirname(__DIR__);
-        }
-
-        public function getInstaller(): ?InstallerInterface
-        {
-            return new Installer();
-        }
-
-        protected function getComposerPackageName(): string
-        {
-            return 'teamneusta/pimcore-presentation-bundle';
-        }
-
-        public function getJsPaths(): array
-        {
-            return [];
-        }
-
-        public function getCssPaths(): array
-        {
-            return [
-                '/bundles/presentation/js/pimcore/startup.js',
-            ];
-        }
-
-        public function getEditmodeJsPaths(): array
-        {
-            return [];
-        }
-
-        public function getEditmodeCssPaths(): array
-        {
-            return [];
-        }
+        return \dirname(__DIR__);
     }
-} else {
-    class NeustaPimcorePresentationBundle extends AbstractPimcoreBundle implements \Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface
+
+    public function getInstaller(): ?InstallerInterface
     {
-        use PackageVersionTrait;
+        return new Installer();
+    }
 
-        public function getPath(): string
-        {
-            return \dirname(__DIR__);
-        }
-
-        public function getInstaller(): ?InstallerInterface
-        {
-            return new Installer();
-        }
-
-        protected function getComposerPackageName(): string
-        {
-            return 'teamneusta/pimcore-presentation-bundle';
-        }
-
-        public function getJsPaths(): array
-        {
-            return [];
-        }
-
-        public function getCssPaths(): array
-        {
-            return [
-                '/bundles/presentation/js/pimcore/startup.js',
-            ];
-        }
-
-        public function getEditmodeJsPaths(): array
-        {
-            return [];
-        }
-
-        public function getEditmodeCssPaths(): array
-        {
-            return [];
-        }
+    protected function getComposerPackageName(): string
+    {
+        return 'teamneusta/pimcore-presentation-bundle';
     }
 }
